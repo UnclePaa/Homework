76d270f10e47a8af046805d53a46267357a05ec7
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-14 18:40:10 +0300

    Update Task_1.java

--- HomeWork2/src/com/homework_2/task_1/Task_1.java
+++ HomeWork2/src/com/homework_2/task_1/Task_1.java
@@ -1,16 +1,16 @@
 package com.homework_2.task_1;
-
+// code -> reformat code или alt+ctrl+L форматирует код
 public class Task_1 {
-
+// пробел не нужен
     public static void main (String[] args) {
-
+// пробел не нужен
         double startDay = 10;
         double everyDay = 10;
         int dayCount;
 
         for (dayCount = 1; dayCount < 7; dayCount++) {
             startDay*=1.1;
-            everyDay+=startDay;}
+            everyDay+=startDay;} << скобку фигурную на новую строку
 
             System.out.println("За 7 дней спортсмен пробежит " + everyDay +" км");
         }
