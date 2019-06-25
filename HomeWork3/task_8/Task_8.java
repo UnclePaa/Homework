b4041129b863825eb195fea97cc1849104589f37
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-25 15:52:59 +0300

    Update Task_8.java

--- HomeWork3/task_8/Task_8.java
+++ HomeWork3/task_8/Task_8.java
@@ -3,7 +3,7 @@ package home_work_3.task_8;
 
 import java.util.Arrays;
 import java.util.*;
-
+// +
 public class Task_8 {
     public static void main(String[] args) {
         int[] arrayOne = new int[10];
@@ -25,5 +25,5 @@ public class Task_8 {
         System.out.println(Arrays.toString(arrayThree));
         System.out.println("Количество целых чисел в третьем массиве = " + l);
     }
-
+// пустая строка, надо убрать
 }
