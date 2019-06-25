0701b5641678338dc8f3459759cb29f3b45faeea
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-25 15:44:51 +0300

    Update Task_3.java

--- HomeWork3/task_3/Task_3.java
+++ HomeWork3/task_3/Task_3.java
@@ -7,6 +7,7 @@ public class Task_3 {
     public static void main(String[] args) {
         int[] arrayRandom = new int[15];
         int i = 0;
+        // arrayRandom.length
         while (i < 15) {
             arrayRandom[i] = (int) (Math.random() * 99);
             i++;
@@ -15,6 +16,8 @@ public class Task_3 {
         System.out.println(arr);
 
         int even = 0;
+        
+        // arrayRandom.length
         for (i = 0; i < 15; i++) {
             arrayRandom[i] %= 2;
             if (arrayRandom[i] == 0)
