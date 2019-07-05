eddb7bfecac96767ee061847bab7180e34f84d3f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-07-05 17:01:17 +0300

    Update Task_8.java

--- homeWorkString/task_8/Task_8.java
+++ homeWorkString/task_8/Task_8.java
@@ -4,6 +4,7 @@ import java.lang.String;
 
 public class Task_8 {
     public static void main(String[] args) {
+        // это должен быть массив Car
         String[][] carStorage = {{"Audi", "1.4"}, {"BMW", "1.8"}};
         for (int i = 0; i < carStorage.length; i++) {
             for (int j = 0; j < carStorage[i].length; j++) {
