571b0063d2815bc58cad945372d24d3738eade36
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-07-16 18:00:28 +0300

    Update ReadTextFromPicture.java

--- homeWork10/task_6/ReadTextFromPicture.java
+++ homeWork10/task_6/ReadTextFromPicture.java
@@ -3,6 +3,8 @@ package com.pickles.practice_10.task_6;
 import java.io.*;
 import java.util.Arrays;
 
+// когда мы "спрятали" наше сообщение в файле, то читать стоит как раз с места, в которое мы его записали. 
+// Получается, надо было запомнить место, начиная с которого будет наше сообщение
 public class ReadTextFromPicture {
     public static final String PIC_PATH = "C:\\Users\\Tsyplik\\IdeaProjects\\Java-core\\src\\com\\pickles\\practice_10\\task_6\\pic.jpg";
 
