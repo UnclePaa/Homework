c625662bc0273671190101d8a69c02fe594507a0
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-07-30 20:01:07 +0300

    Update Task_5.java

--- homeWork14/task_5/Task_5.java
+++ homeWork14/task_5/Task_5.java
@@ -8,6 +8,7 @@ public class Task_5 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(16, 7, 11, 9, 5, 190, 10);
 
+        // multiplyFirstFiveElem -> result. Не привязывай название переменной к действиям внутри
         Optional<Integer> multiplyFirstFiveElem = numbers.stream()
                 .limit(5)
                 .reduce((x, y) -> x * y);
