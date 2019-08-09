985e6057c0e47424b79e1c973a01cc5ffe72c8d0
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-08-09 20:15:12 +0300

    Update Task_4_CheckCodeMain.java

--- homeWork17/task_4/Task_4_CheckCodeMain.java
+++ homeWork17/task_4/Task_4_CheckCodeMain.java
@@ -3,6 +3,7 @@ package com.pickles.practice_17.task_4;
 import java.util.LinkedList;
 import java.util.List;
 
+//+
 public class Task_4_CheckCodeMain {
     public static void main(String[] args) {
         List<String> fruitsAndBerries = new LinkedList<>();
