ed5577385b65976908b0843e0df7b66fe510f1a3
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-08-15 21:57:56 +0300

    Update AddElemToList.java

--- homework_17_2/task_1/AddElemToList.java
+++ homework_17_2/task_1/AddElemToList.java
@@ -7,6 +7,7 @@ public class AddElemToList {
 
     ReentrantLock reentrantLock = new ReentrantLock();
 
+    // List<Integer> integerList - зачем он тебе в качестве аргумента? это должно быть полем этого класса
     public void addNewElem(List<Integer> integerList, int element) {
         reentrantLock.lock();
         integerList.add(element);
