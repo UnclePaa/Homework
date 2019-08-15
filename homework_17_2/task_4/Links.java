14dcd4f196b44b52b27e5eed32f8bce63122eef5
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-08-15 22:04:40 +0300

    Update Links.java

--- homework_17_2/task_4/Links.java
+++ homework_17_2/task_4/Links.java
@@ -1,5 +1,6 @@
 package com.pickles.practice_17_2.task_4;
 
+// не стоит делать класс для констант. либо в методе мейн, либо аргументом программы
 public class Links {
     private static final String SOURCE_FOLDER = "C:\\Users\\Tsyplik\\IdeaProjects\\Java-core\\src\\com\\pickles\\practice_17_2\\task_4\\txt_Files";
     private static final String FILE_1_PATH = "C:\\Users\\Tsyplik\\IdeaProjects\\Java-core\\src\\com\\pickles\\practice_17_2\\task_4\\txt_Files\\file_1.txt";
