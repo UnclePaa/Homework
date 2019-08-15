b7adc73b02a43cdb07ca4ae40b286118ee75c3c4
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-08-15 22:07:36 +0300

    Update Links.java

--- homework_17_2/task_6/Links.java
+++ homework_17_2/task_6/Links.java
@@ -1,5 +1,6 @@
 package com.pickles.practice_17_2.task_6;
 
+//  не делай классы только с константами
 public class Links {
     private static final String FOLDER_PATH = "C:\\Users\\Tsyplik\\IdeaProjects\\Java-core\\src\\com\\pickles\\practice_17_2\\task_6\\test_folder\\";
 
